d5b6603b2b5a9d3b6fa8641249d77540bd465e20
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-09 05:17:00 +0300

    adding_object_to_schema

--- models/post.js
+++ models/post.js
@@ -2,4 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const postSchema = new Schema({
-})
+text:{
+    type: String,
+    required: true,
+},
+title:{
+    type: String,
+    required: true,
+},
+author:{
+    type: String,
+    required: true,
+},
+}, {timestamps:true});
