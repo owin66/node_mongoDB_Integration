54dd715b1fc04405edc5635f4fabc0da666a0616
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-12 13:18:24 +0300

    refactoring

--- server.js
+++ server.js
@@ -62,6 +62,30 @@ app.get('/posts/:id', (req, res) => {
         })
 })
 
+
+app.get('/edit/:id', (req, res) => {
+    const title = 'Edit Post';
+    Post
+        .findById(req.params.id)
+        .then(post => res.render(createPath('edit-post'), { post, title }))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
+
+app.put('/edit/:id', (req, res) => {
+    const { title, author, text } = req.body;
+    const { id } = req.params;
+    Post
+        .findByIdAndUpdate(id, { title, author, text })
+        .then((result) => res.redirect(`/posts/${id}`))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
+
 app.get('/posts', (req, res) => {
     const title = 'Posts';
     Post
