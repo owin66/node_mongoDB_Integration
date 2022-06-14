5791e8cf672aa993e1cb637f9a1c72c9ea599216
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-14 21:58:21 +0300

    refactoring_api_post_controller

--- controllers/api-post-controller.js
+++ controllers/api-post-controller.js
@@ -1,86 +1,65 @@
 const Post = require('../models/post');
-const createPath = require('../helpers/create-path')
 const Contact = require("../models/contact");
 
 const handleError = (res, error) => {
-    console.log(error);
-    res.render(createPath('error'), {title: 'Error'})
+    res.status(500).send(error);
 }
 
 const getPost = (req, res) => {
-    const title = 'Post';
     Post
         .findById(req.params.id)
-        .then((post) => res.render(createPath('post'), {post, title}))
+        .then((post) => res.status(200).json(post))
         .catch((error) => handleError(res, error));
 }
 
 const deletePost = (req, res) => {
-    const title = 'Post';
     Post
         .findByIdAndDelete(req.params.id)
-        .then((result) => {
-            res.sendStatus(200)
-        })
+        .then(() => res.status(200).json(req.params.id))
         .catch((error) => handleError(res, error));
 }
 
-const getEditPost = (req, res) => {
-    const title = 'Edit Post';
-    Post
-        .findById(req.params.id)
-        .then(post => res.render(createPath('edit-post'), {post, title}))
-        .catch((error) => handleError(res, error));
-}
 
 const editPost = (req, res) => {
     const {title, author, text} = req.body;
     const {id} = req.params;
     Post
         .findByIdAndUpdate(id, {title, author, text})
-        .then((result) => res.redirect(`/posts/${id}`))
+        .then((post) => res.status(200).json(post))
         .catch((error) => handleError(res, error));
 }
 
 const getPosts = (req, res) => {
-    const title = 'Posts';
     Post
         .find()
         .sort({createdAt: -1})
-        .then((posts) => res.render(createPath('posts'), {posts, title}))
+        .then((posts) => res.status(200).json(posts))
         .catch((error) => handleError(res, error));
 }
 
-const getAddPost = (req, res) => {
+
+const addPost = (req, res) => {
     const {title, author, text} = req.body;
     const post = new Post({title, author, text});
     post
         .save()
-        .then((result) => res.redirect('/posts'))
+        .then((post) => res.status(200).json(post))
         .catch((error) => handleError(res, error));
-
-}
-
-const addPost = (req, res) => {
-    const title = 'Add post';
-    res.render(createPath('add-post'), {title}) //путь
 }
 
 const getContacts = (req, res) => {
     const title = 'Contacts';
     Contact
         .find()
-        .then((contacts) => res.render(createPath('contacts'), {contacts, title}))
+        .then()
         .catch((error) => handleError(res, error));
 }
 
 module.exports = {
     getPost,
     deletePost,
-    getEditPost,
     editPost,
     getPosts,
-    getAddPost,
     addPost,
     getContacts
 }
