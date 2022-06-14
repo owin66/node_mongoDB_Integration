49c674010f93933eb35f2c01ac1396d329fc78de
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-14 19:29:07 +0300

    finish_refactoring

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -25,7 +25,64 @@ const deletePost = (req, res) => {
         })
 }
 
+const getEditPost = (req, res) => {
+    const title = 'Edit Post';
+    Post
+        .findById(req.params.id)
+        .then(post => res.render(createPath('edit-post'), {post, title}))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), {title: 'Error'});
+        });
+}
+
+const editPost = (req, res) => {
+    const {title, author, text} = req.body;
+    const {id} = req.params;
+    Post
+        .findByIdAndUpdate(id, {title, author, text})
+        .then((result) => res.redirect(`/posts/${id}`))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), {title: 'Error'});
+        });
+}
+
+const getPosts = (req, res) => {
+    const title = 'Posts';
+    Post
+        .find()
+        .sort({createdAt: -1})
+        .then((posts) => res.render(createPath('posts'), {posts, title}))
+        .catch((error) => {
+            console.log(error)
+            res.render(createPath('error'), {title: 'Error'})
+        })
+}
+
+const getAddPost = (req, res) => {
+    const {title, author, text} = req.body;
+    const post = new Post({title, author, text});
+    post
+        .save()
+        .then((result) => res.redirect('/posts'))
+        .catch((error) => {
+            console.log(error)
+            res.render(createPath('error'), {title: 'Error'})
+        })
+
+}
+const addPost = (req, res) => {
+    const title = 'Add post';
+    res.render(createPath('add-post'), {title}) //путь
+}
+
 module.exports = {
     getPost,
     deletePost,
+    getEditPost,
+    editPost,
+    getPosts,
+    getAddPost,
+    addPost,
 }
--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,65 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const {getPost, deletePost} = require('../controllers/post-controller')
-
-
+const {
+    getPost,
+    deletePost,
+    getEditPost,
+    editPost,
+    getPosts,
+    getAddPost,
+    addPost,
+} = require('../controllers/post-controller')
 
 
 router.get('/posts/:id', getPost)
-
 router.delete('/posts/:id', deletePost)
-
-
-router.get('/edit/:id', (req, res) => {
-    const title = 'Edit Post';
-    Post
-        .findById(req.params.id)
-        .then(post => res.render(createPath('edit-post'), {post, title}))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-router.put('/edit/:id', (req, res) => {
-    const {title, author, text} = req.body;
-    const {id} = req.params;
-    Post
-        .findByIdAndUpdate(id, {title, author, text})
-        .then((result) => res.redirect(`/posts/${id}`))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-router.get('/posts', (req, res) => {
-    const title = 'Posts';
-    Post
-        .find()
-        .sort({createdAt: -1})
-        .then((posts) => res.render(createPath('posts'), {posts, title}))
-        .catch((error) => {
-            console.log(error)
-            res.render(createPath('error'), {title: 'Error'})
-        })
-})
-router.post('/add-post', (req, res) => {
-    const {title, author, text} = req.body;
-    const post = new Post({title, author, text});
-    post
-        .save()
-        .then((result) => res.redirect('/posts'))
-        .catch((error) => {
-            console.log(error)
-            res.render(createPath('error'), {title: 'Error'})
-        })
-
-})
-
-router.get('/add-post', (req, res) => {
-    const title = 'Add post';
-    res.render(createPath('add-post'), {title}) //путь
-})
+router.get('/edit/:id', getEditPost)
+router.put('/edit/:id', editPost)
+router.get('/posts', getPosts)
+router.post('/add-post', getAddPost)
+router.get('/add-post', addPost)
 
 module.exports = router;
