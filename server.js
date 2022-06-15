37aa78059025b75a0720ab1934000bb9f40d7261
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-15 21:50:45 +0300

    fix_bag

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const chalk = require('chalk');
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
@@ -6,6 +7,10 @@ const postRoutes = require('./routes/post-routes')
 const postApiRoutes = require('./routes/api-post-routes')
 const contactRoutes = require('./routes/contact-routes')
 const createPath = require('./helpers/create-path')
+
+const errorMsg = chalk.bgKeyword('white').redBright;
+const successMsg = chalk.bgKeyword('green').white;
+
 
 const app = express();
 
@@ -17,16 +22,16 @@ const PORT = 3000;
 const db = 'mongodb+srv://owin66:Pass321@nodejs.tyesk.mongodb.net/node-blog?retryWrites=true&w=majority'
 
 mongoose
-    .connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then((res) => console.log('Connect to DB'))
-    .catch((error) => console.log(error));
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((res) => console.log(successMsg('Connect to DB')))
+    .catch((error) => console.log(errorMsg(error)));
 
 
 //функция перехода путей
 
 
 app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`);
+    error ? console.log(errorMsg(error)) : console.log(successMsg(`listening port ${PORT}`));
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
